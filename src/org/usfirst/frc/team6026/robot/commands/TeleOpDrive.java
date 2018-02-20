d99d1a4e4acbe48adb26738846b5dd79dc30a637
stem <stem@BAHS-STEM-02.ba.k12.ok.us>
2018-02-20 09:43:32 -0600

    teleop cleanup

--- src/org/usfirst/frc/team6026/robot/commands/TeleOpDrive.java
+++ src/org/usfirst/frc/team6026/robot/commands/TeleOpDrive.java
@@ -22,10 +22,19 @@ public class TeleOpDrive extends Command {
 		double jY = OI.driveJoystick.getY();
 		double jX = OI.driveJoystick.getRawAxis(4);
 		double sY = OI.supportJoystick.getRawAxis(5);
+		double rL = Robot.kRangeFinderSubsystem.getLeftRange();
+		double rR = Robot.kRangeFinderSubsystem.getRightRange();
+		double lTrigger = OI.driveJoystick.getRawAxis(2);
+		double rTrigger = OI.driveJoystick.getRawAxis(3);
 		
+		SmartDashboard.putNumber("JoyX", jX);
+		SmartDashboard.putNumber("JoyY", jY);
 		
-		Robot.kLiftSubsystem.driveLiftMotor(sY/2);	// Drive lift up/down
+		// Lift
+		Robot.kLiftSubsystem.driveLiftMotor(sY/2);
+		SmartDashboard.putNumber("sY: ", sY);
 		
+		// Gripper
 		if( OI.supportJoystick.getRawAxis(3) > 0.01) {	// Capture
 			Robot.kGripperSubsystem.driveGripperMotor(((OI.supportJoystick.getRawAxis(3)/2)+0.5));
 		}else if( OI.supportJoystick.getRawAxis(2) > 0.01) {	// Eject
@@ -36,27 +45,24 @@ public class TeleOpDrive extends Command {
 			Robot.kGripperSubsystem.driveGripperMotor(0.0);
 		}
 		
+		// Tilt
 		if(OI.driveJoystick.getRawButton(3)) {
 			Robot.kGripperSubsystem.driveTiltMotor(0.5);
 		} else if(OI.driveJoystick.getRawButton(4)) {
 			Robot.kGripperSubsystem.driveTiltMotor(-0.5);
 		}
-			
-		SmartDashboard.putNumber("JoyX", jX);
-		SmartDashboard.putNumber("JoyY", jY);
 		
-		double speed1 = OI.driveJoystick.getRawAxis(2);
-		double speed2 = OI.driveJoystick.getRawAxis(3);
-		
-		if(speed2 > 0.1) {
-			Robot.kDriveTrainSubsystem.drive(speed2, jX);
-		}else if(speed1 > 0.1) {
-			Robot.kDriveTrainSubsystem.drive(-speed1, jX);
+		// Drive
+		if(rTrigger > 0.1) {
+			Robot.kDriveTrainSubsystem.drive(rTrigger, jX);
+		}else if(lTrigger > 0.1) {
+			Robot.kDriveTrainSubsystem.drive(-lTrigger, jX);
 		}else {
 			Robot.kDriveTrainSubsystem.drive(0, jX);
 		}
 		
-		SmartDashboard.putNumber("Lift (mm)", Robot.kLiftSubsystem.getLiftMotorPosition());
+		// LIFT ENCODER REMOVED
+		// SmartDashboard.putNumber("Lift (mm)", Robot.kLiftSubsystem.getLiftMotorPosition());
 	}
 	
 	protected void end() {
